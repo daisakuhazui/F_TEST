66f03812af9beebea6e91d54faf59199445af530
DaisakuHazui <daisakuhazui@Vino-MAC.local>
2018-09-07 19:30:42 +0900

    Create models

--- eb/fescore/fesweb/models.py
+++ eb/fescore/fesweb/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
+    title = models.CharField(max_length=20)
+    content = models.TextField()
